a5874ced61726ad60080afe274b535de63a3b7d8
MiguelG64B <79877254+MiguelG64B@users.noreply.github.com>
2021-11-08 14:45:27 -0500

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,19 @@
+
+   <footer>
+        <div class="bg-light py-3">
+      <div class="container">
+        <div class="row">
+        <div class="col-md-12 mb-0"><a >NAVEGACION<span class="mx-2 mb-0"></span>
+           <strong ><a class="text-black" href="index.php">Principal</a></strong>
+           <span class="mx-2 mb-0"></span>
+           <strong ><a class="text-black" href="sobrenosotros.php">Sobre nosotros</a></strong>
+           <span class="mx-2 mb-0"></span>
+           <strong ><a class="text-black" href="contacto.php">Contacto</a></strong>
+           <div class="container text-right">
+            <strong ><a href="index.php">Ayuda y documentacion</a></strong>
+          </div> 
+        </div>
+        </div>
+      </div>
+      </div> 
+    </footer>
