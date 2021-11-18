350cdd1af0c53b67600c39a04387ab2fef1defba
MiguelG64B <79877254+MiguelG64B@users.noreply.github.com>
2021-11-18 12:29:08 -0500

    Delete php directory

--- php/cerrar_sesion.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();
-unset($_SESSION['datos_login']);
-header("Location: ../index.php");
-
-?>
--- php/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $servidor="localhost";
-    $nombreBd="proyectouni1";
-    $usuario="root";
-    $pass="";
-    $conexion = new mysqli($servidor,$usuario,$pass,$nombreBd);
-?>
